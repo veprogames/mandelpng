0951c6630ba1c79f7143390867f3c8ae90a5e47a
veprogames <75524847+veprogames@users.noreply.github.com>
2024-11-24 20:41:29 +0100

    create image of higher size

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use std::{fs::File, io::BufWriter, path::Path, error::Error};
 
+const W: usize = 640;
+const H: usize = 360;
+
+fn get_image_data() -> Vec<u8> {
+    let mut data = [0u8; W * H * 3];
+
+    for y in 0..H {
+        for x in 0..W {
+            let idx = (x + y * W) * 3;
+            data[idx] = 255;
+            data[idx + 1] = 255;
+            data[idx + 2] = 255;
+        }
+    }
+
+    data.to_vec()
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let path = Path::new("result.png");
     let file = File::create(path)?;
     let bw = BufWriter::new(file);
 
-    let mut encoder = png::Encoder::new(bw, 1, 1);
+    let mut encoder = png::Encoder::new(bw, W as u32, H as u32);
     encoder.set_color(png::ColorType::Rgb);
     encoder.set_depth(png::BitDepth::Eight);
 
     let mut writer = encoder.write_header()?;
 
-    let data = &[255, 255, 255];
+    let data = &get_image_data();
 
     writer.write_image_data(data)?;
 

886af0390c4f3d7e3eae1c9f98e4b675b40e3c43
veprogames <75524847+veprogames@users.noreply.github.com>
2024-11-24 20:30:03 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-use std::{fs::File, io::BufWriter, path::Path};
+use std::{fs::File, io::BufWriter, path::Path, error::Error};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let path = Path::new("result.png");
-    let file = File::create(path)
-        .expect("File created");
+    let file = File::create(path)?;
     let bw = BufWriter::new(file);
 
     let mut encoder = png::Encoder::new(bw, 1, 1);
     encoder.set_color(png::ColorType::Rgb);
     encoder.set_depth(png::BitDepth::Eight);
 
-    let mut writer = encoder.write_header()
-        .expect("Add PNG header to Buffer");
+    let mut writer = encoder.write_header()?;
 
     let data = &[255, 255, 255];
 
-    writer.write_image_data(data)
-        .expect("Write Image data to buffer");
+    writer.write_image_data(data)?;
 
     println!("fin");
+    
+    Ok(())
 }
